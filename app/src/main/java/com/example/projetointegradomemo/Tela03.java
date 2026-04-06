057d940fb0a65b7480302c5381a92d7f7bdbf17d
erickplayer1 <erick.victor@alunos.ifsuldeminas.edu.br~>
2026-04-06 11:32:01 -0300

    Versao 30/03 0.2

--- app/src/main/java/com/example/projetointegradomemo/Tela03.java
+++ app/src/main/java/com/example/projetointegradomemo/Tela03.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,12 +21,13 @@ import java.util.Collections;
 
 public class Tela03 extends AppCompatActivity implements View.OnClickListener, Runnable {
     private TextView textoDoNome;
-    private ImageView img1, img2, img3, img4, img5, img6;
+    private ImageView img1, img2, img3, img4, img5, img6 , img7, img8;
     private ImageView imgTocado1, imgTocado2;
     private int imagemImgTocado1, imagemImgTocado2, contaToque;
     private Handler handler;
 
     private ArrayList<Integer> lista;
+    private Button fim, de_novo;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -48,6 +50,9 @@ public class Tela03 extends AppCompatActivity implements View.OnClickListener, R
         img4 = findViewById(R.id.imageView5);
         img5 = findViewById(R.id.imageView11);
         img6 = findViewById(R.id.imageView13);
+        img7 = findViewById(R.id.imageView14);
+        img8 = findViewById(R.id.imageView12);
+
 
         lista = new ArrayList<Integer>();
         lista.add(R.drawable.ic_action_name_3);
@@ -59,13 +64,7 @@ public class Tela03 extends AppCompatActivity implements View.OnClickListener, R
         lista.add(R.drawable.ic_action_name_6);
         lista.add(R.drawable.ic_action_name_6);
 
-        Collections.shuffle(lista);
-        img1.setImageResource(lista.get(0));
-        img2.setImageResource(lista.get(1));
-        img3.setImageResource(lista.get(2));
-        img4.setImageResource(lista.get(3));
-        img5.setImageResource(lista.get(4));
-        img6.setImageResource(lista.get(5));
+
 
         img1.setOnClickListener(this);
         img2.setOnClickListener(this);
@@ -73,14 +72,57 @@ public class Tela03 extends AppCompatActivity implements View.OnClickListener, R
         img4.setOnClickListener(this);
         img5.setOnClickListener(this);
         img6.setOnClickListener(this);
+        img7.setOnClickListener(this);
+        img8.setOnClickListener(this);
 
         handler = new Handler();
-        handler.postDelayed(this, 3000);
-        contaToque = 0;
         imgTocado1 = new ImageView(this);
         imgTocado2 = new ImageView(this);
+
+        fim = findViewById(R.id.button4);
+        fim.setOnClickListener(this);
+        de_novo = findViewById(R.id.button3);
+        de_novo.setOnClickListener(this);
+        inicio();
+
+
     }
 
+    public void inicio (){
+        Collections.shuffle(lista);
+        img1.setImageResource(lista.get(0));
+        img2.setImageResource(lista.get(1));
+        img3.setImageResource(lista.get(2));
+        img4.setImageResource(lista.get(3));
+        img5.setImageResource(lista.get(4));
+        img6.setImageResource(lista.get(5));
+        img7.setImageResource(lista.get(6));
+        img8.setImageResource(lista.get(7));
+
+        img1.setEnabled(true);
+        img2.setEnabled(true);
+        img3.setEnabled(true);
+        img4.setEnabled(true);
+        img5.setEnabled(true);
+        img6.setEnabled(true);
+        img7.setEnabled(true);
+        img8.setEnabled(true);
+
+
+        img1.setBackgroundColor(Color.parseColor("#3868FF"));
+        img2.setBackgroundColor(Color.parseColor("#3868FF"));
+        img3.setBackgroundColor(Color.parseColor("#3868FF"));
+        img4.setBackgroundColor(Color.parseColor("#3868FF"));
+        img5.setBackgroundColor(Color.parseColor("#3868FF"));
+        img6.setBackgroundColor(Color.parseColor("#3868FF"));
+        img7.setBackgroundColor(Color.parseColor("#3868FF"));
+        img8.setBackgroundColor(Color.parseColor("#3868FF"));
+
+        handler.postDelayed(this, 3000);
+        contaToque = 0;
+
+
+    }
     public void compara(int imagem1, int imagem2){
         contaToque =0;
         if(imagem1 == imagem2)
@@ -98,8 +140,8 @@ public class Tela03 extends AppCompatActivity implements View.OnClickListener, R
             h1.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    imgTocado1.setBackgroundColor(Color.WHITE);
-                    imgTocado2.setBackgroundColor(Color.WHITE);
+                    imgTocado1.setBackgroundColor(Color.parseColor("#3868FF"));
+                    imgTocado2.setBackgroundColor(Color.parseColor("#3868FF"));
                     imgTocado1.setImageResource(R.drawable.ic_action_name_2);
                     imgTocado2.setImageResource(R.drawable.ic_action_name_2);
 
@@ -187,6 +229,42 @@ public class Tela03 extends AppCompatActivity implements View.OnClickListener, R
                 compara(imagemImgTocado1,imagemImgTocado2);
             }
         }
+        if(view == img7){
+            img7.setImageResource(lista.get(6));
+            if(contaToque == 1)
+            {
+                imgTocado1 = img7;
+                imagemImgTocado1 = lista.get(6);
+            }else {
+                imgTocado2 = img7;
+                imagemImgTocado2 = lista.get(6);
+                compara(imagemImgTocado1,imagemImgTocado2);
+            }
+        }
+        if(view == img8){
+            img8.setImageResource(lista.get(7));
+            if(contaToque == 1)
+            {
+                imgTocado1 = img8;
+                imagemImgTocado1 = lista.get(7);
+            }else {
+                imgTocado2 = img8;
+                imagemImgTocado2 = lista.get(7);
+                compara(imagemImgTocado1,imagemImgTocado2);
+            }
+        }
+        if(view == fim){
+            Intent i = new Intent(this, Tela04.class);
+            Bundle caixa = new Bundle();
+            caixa.putString("nome", textoDoNome.getText().toString());
+            i.putExtras(caixa);
+            startActivity(i);
+
+        }
+        if(view == de_novo){
+            inicio();
+        }
+
     }
 
     @Override
@@ -197,8 +275,13 @@ public class Tela03 extends AppCompatActivity implements View.OnClickListener, R
         img4.setImageResource(R.drawable.ic_action_name_2);
         img5.setImageResource(R.drawable.ic_action_name_2);
         img6.setImageResource(R.drawable.ic_action_name_2);
+        img7.setImageResource(R.drawable.ic_action_name_2);
+        img8.setImageResource(R.drawable.ic_action_name_2);
+
 
 
 
     }
+
+
 }
--- app/src/main/java/com/example/projetointegradomemo/Tela04.java
+++ app/src/main/java/com/example/projetointegradomemo/Tela04.java
@@ -1,6 +1,8 @@
 package com.example.projetointegradomemo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Tela04 extends AppCompatActivity {
+    private TextView textoDoNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela04);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        textoDoNome = findViewById(R.id.textView4);
+        Intent i = getIntent();
+        if(i != null){
+            Bundle caixa = new Bundle();
+            caixa = i.getExtras();
+            if(caixa != null){
+                textoDoNome.setText(caixa.getString("nome"));
+            }
+        }
     }
 }
